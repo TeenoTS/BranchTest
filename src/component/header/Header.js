0638792269f3071507831d6f4941a487ea51f83f
Teeno <tino_08478@hotmail.com>
2023-03-22 13:51:15 +0700

    start project

---
+++ src/component/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className='center bg-1'>
+      <button>Click me!!!</button>
+    </div>
+  );
+}
+
+export default Header;
